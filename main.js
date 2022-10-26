54c1b68bc03c4245cbb5579800f87b85cfbd1bc6
sindre-kodehode <sindre.kodehode@gmail.com>
2022-10-26 22:36:13 +0200

    use index buffer

--- main.js
+++ main.js
@@ -1,58 +1,5 @@
-/*******************************************************************************
-*  vertex shader                                                               *
-*******************************************************************************/
-const vertexShaderSource =
-`#version 300 es
-
-in vec2 a_Position;
-
-uniform vec2 u_Resolution;
-
-void main() {
-  vec2 zeroToOne = a_Position / u_Resolution;
-  vec2 zeroToTwo = zeroToOne * 2.0;
-  vec2 clipSpace = zeroToTwo - 1.0;
-
-  gl_Position = vec4( clipSpace * vec2( 1, -1 ), 0, 1 );
-}
-`;
-
-
-/*******************************************************************************
-*  fragment shader                                                             *
-*******************************************************************************/
-const fragmentShaderSource =
-`#version 300 es
-
-precision highp float;
-
-uniform vec4 u_Color;
-
-out vec4 outColor;
-
-void main() {
-  outColor = u_Color;
-}
-`;
-
-
-/*******************************************************************************
-*  compile shader                                                              *
-*******************************************************************************/
-const createShader = ( gl, type, source ) => {
-  const shader = gl.createShader( type );
-
-  gl.shaderSource( shader, source );
-  gl.compileShader( shader );
-
-  const success = gl.getShaderParameter( shader, gl.COMPILE_STATUS );
-
-  if ( success ) return shader;
-
-  console.error( gl.getShaderInfoLog( shader ) );
-  gl.deleteShader( shader );
-}
-
+import { vsSource, fsSource } from "./shaders.js";
+import { VertexShader, FragmentShader } from "./Shader.js";
 
 /*******************************************************************************
 *  create program                                                              *
@@ -65,35 +12,12 @@ const createProgram = ( gl, vertexShader, fragmentShader ) => {
   gl.linkProgram( program );
 
   const success = gl.getProgramParameter( program, gl.LINK_STATUS );
-  
+
   if ( success ) return program;
 
   console.error( gl.getProgramInfoLog( program ) );
   gl.deleteProgram( program );
 };
-
-/*******************************************************************************
-*  create rectangle                                                            *
-*******************************************************************************/
-const createRectangle = ( gl, x, y, width, height ) => {
-  const x1 = x;
-  const x2 = x + width;
-  const y1 = y;
-  const y2 = y + height;
-
-  gl.bufferData(
-    gl.ARRAY_BUFFER,
-    new Float32Array([
-      x1, y1,
-      x2, y1,
-      x1, y2,
-      x1, y2,
-      x2, y1,
-      x2, y2,
-    ]),
-    gl.STATIC_DRAW
-  );
-}
 
 /*******************************************************************************
 *  main                                                                        *
@@ -107,56 +31,39 @@ const main = () => {
     return;
   }
 
-  const vertexShader = createShader(
-    gl,
-    gl.VERTEX_SHADER,
-    vertexShaderSource
-  );
+  // *** initialize viewport *** //
+  gl.viewport( 0, 0, gl.canvas.width, gl.canvas.height );
+  gl.clearColor( 0, 0, 0, 1.0 )  ;
+  gl.clear( gl.COLOR_BUFFER_BIT );
 
-  const fragmentShader = createShader(
-    gl,
-    gl.FRAGMENT_SHADER,
-    fragmentShaderSource
-  );
+  // *** compile shaders and create program *** //
+  const vertexShader   = new VertexShader( gl, vsSource );
+  const fragmentShader = new FragmentShader( gl, fsSource );
 
   const program = createProgram(
     gl,
-    vertexShader,
-    fragmentShader
+    vertexShader.compile(),
+    fragmentShader.compile()
   );
 
-  const resolutionUniformLocation = gl.getUniformLocation(
-    program,
-    "u_Resolution"
-  );
+  gl.useProgram( program );
 
-  const positionAttributeLocation = gl.getAttribLocation(
-    program,
-    "a_Position"
-  );
-
-  const colorAttributeLocation = gl.getUniformLocation(
-    program,
-    "u_Color"
-  );
+  // *** position buffer *** //
+  const positions = new Float32Array([
+    -0.5, -0.5,
+     0.5, -0.5,
+     0.5,  0.5,
+    -0.5,  0.5,
+  ]);
 
   const positionBuffer = gl.createBuffer();
   gl.bindBuffer( gl.ARRAY_BUFFER, positionBuffer );
-
-  const positions = new Float32Array([
-    10, 20,
-    80, 20,
-    10, 30,
-    10, 30,
-    80, 20,
-    80, 30,
-  ]);
-
   gl.bufferData( gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW );
 
-  const vertexArrayObject = gl.createVertexArray();
-  gl.bindVertexArray( vertexArrayObject );
-  gl.enableVertexAttribArray( positionAttributeLocation );
+  // *** vertex array object *** //
+  const vertexArray = gl.createVertexArray();
+  gl.bindVertexArray( vertexArray );
+  gl.enableVertexAttribArray( 0 );
 
   {
     const size      = 2;
@@ -166,7 +73,7 @@ const main = () => {
     const offset    = 0;
 
     gl.vertexAttribPointer(
-      positionAttributeLocation,
+      0,
       size,
       type,
       normalize,
@@ -175,41 +82,24 @@ const main = () => {
     );
   }
 
-  gl.viewport( 0, 0, gl.canvas.width, gl.canvas.height );
-  gl.clearColor( 0, 0, 0, 1.0 )  ;
-  gl.clear( gl.COLOR_BUFFER_BIT );
-  gl.useProgram( program );
+  // *** index buffer object *** //
+  const indices = new Uint32Array([
+    0, 1, 2,
+    2, 3, 0,
+  ]);
 
-  gl.uniform2f(
-    resolutionUniformLocation,
-    gl.canvas.width,
-    gl.canvas.height
-  );
+  const indexBuffer = gl.createBuffer();
+  gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, indexBuffer );
+  gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW );
 
-  const randomInt = range => Math.floor( Math.random() * range );
-
-  for ( let i = 0; i < 50; i++ ) {
-    createRectangle(
-      gl,
-      randomInt( 300 ),
-      randomInt( 300 ),
-      randomInt( 300 ),
-      randomInt( 300 ),
-    )
-
-    gl.uniform4f(
-      colorAttributeLocation,
-      Math.random(),
-      Math.random(),
-      Math.random(),
-      1.0
-    );
-
+  // *** draw scene *** //
+  {
     const primitiveType = gl.TRIANGLES;
     const offset        = 0;
     const count         = 6;
+    const indexType     = gl.UNSIGNED_INT;
 
-    gl.drawArrays( primitiveType, offset, count );
+    gl.drawElements( primitiveType, count, indexType, offset);
   }
 };
 
